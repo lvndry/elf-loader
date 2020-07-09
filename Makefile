f16599aed2c75463a22b62fa63d879266e8875ef
lvndry <monga.4landry@gmail.com>
2020-07-10 00:43:31 +0200

    trivial elf loader

--- Makefile
+++ Makefile
@@ -7,8 +7,11 @@ OBJS= ${SRC:.c=.o}
 
 all: elf-loader clean
 
-elf-loader: ${OBJS}
-	${CC} -o $@ $^ ${LDFLAGS}
+elf-loader: ${OBJS} execute
+	${CC} -o $@ $< execute.o ${LDFLAGS}
+
+execute:
+	${CC} -c src/execute.S -o execute.o
 
 %.o: %.c
 	${CC} -o $@ -c $< ${CFLAGS}
--- src/elf-loader.c
+++ src/elf-loader.c
@@ -1,4 +1,4 @@
-#define _GNU_SOURCE
+#define _GNU_SOURCE // DELETE
 
 #include <elf.h>
 #include <err.h>
@@ -12,12 +12,20 @@
 #include <stdio.h> // DELETE BEFORE PUSH
 
 #include "elf-loader.h"
+#include "execute.h"
 
 // DELETE
 void show_prog_mapping(void) {
   char *pidmaps = NULL;
   asprintf(&pidmaps, "cat /proc/%u/maps", getpid());
   system(pidmaps);
+}
+
+unsigned long *get_stack_ptr(void) {
+  register unsigned long *ptr __asm("rsp");
+  __asm("mov %rsp, %rax");
+
+  return ptr;
 }
 
 // DELETE
@@ -30,7 +38,7 @@ size_t roundUp(long unsigned int num, long m) {
   return ((num + m - 1) / m) * m;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[], char *envp[]) {
   if (argc < 2) {
     errx(MISSING_ARGS, "Missing argument");
   }
@@ -69,23 +77,56 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < header.e_phnum; ++i) {
     read(elf, &ph, sizeof(Elf64_Phdr));
 
-    if (ph.p_type != PT_LOAD) {
+    if (ph.p_type != PT_LOAD && ph.p_type != PT_GNU_STACK) {
+      continue;
+    }
+
+    if (!ph.p_memsz) {
       continue;
     }
 
     printf("memsz: %ld -- roundup: %ld\n", ph.p_memsz,
            roundUp(ph.p_memsz, PAGE_SIZE));
 
-    void *seg_space =
-        mmap((void *)align(ph.p_vaddr), roundUp(ph.p_memsz, PAGE_SIZE),
-             PROT_EXEC | PROT_READ | PROT_WRITE, MAP_PRIVATE, elf,
-             (off_t)align(ph.p_offset));
+    void *seg_space = mmap((void *)ph.p_vaddr, roundUp(ph.p_memsz, PAGE_SIZE),
+                           PROT_EXEC | PROT_READ | PROT_WRITE, MAP_PRIVATE, elf,
+                           (off_t)align(ph.p_offset));
 
     if (seg_space == MAP_FAILED) {
-      print_errno();
       errx(MEM_ERROR, "Could not map segment in memory");
     }
+
+    // read-only
+    if (!(ph.p_flags & PF_W)) {
+      mprotect(seg_space, ph.p_memsz, PROT_READ);
+    }
+
+    // executable implies read
+    if (ph.p_flags & PF_X) {
+      mprotect(seg_space, ph.p_memsz, PROT_EXEC);
+    }
   }
+
+  Elf64_auxv_t *auxv;
+
+  // envp is null terminated and auxv is right after
+  while (*envp++ != NULL)
+    ;
+
+  uintptr_t entrypoint;
+  for (auxv = (Elf64_auxv_t *)envp; auxv->a_type != AT_NULL; auxv++) {
+    if (auxv->a_type == AT_ENTRY) {
+      entrypoint = auxv->a_un.a_val;
+    }
+  }
+  entrypoint = entrypoint;
+
+  int *stack_argc = (void *)&argc;
+  *((int *)stack_argc) = argc - 1;
+  char ***stack_argv = (char ***)stack_argc + 1;
+  *stack_argv = (char **)(argv + 1);
+
+  execute((void *)stack_argc, header.e_entry);
 
   show_prog_mapping();
 
